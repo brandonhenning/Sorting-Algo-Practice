c45c4a872af2d5b915e6feefe0f999a77ce82813
Brandon Henning <brandonhenning@Brandons-MacBook-Pro-3.local>
2018-05-23 14:07:22 -0600

    added insertion algo file

--- insertion.js
+++ insertion.js
@@ -1,14 +1,21 @@
-let unsortedArray = [2,4,1,6]
+let unsortedArray1 = [2,4,1,6]
+let unsortedArray2 = [2,4,1,78,333,22,2,98,-92,5]
+let unsortedArray3 = [3,2,1,0,8,7,2,1,4,6,8000]
 
 function insertSort (array) {
-    array.forEach((number, index) => {
-        if (index !== 0) {
-            if (number > number[index - 1]) {
-              console.log('found one')
-            }
+    for (let i = 1; i < array.length; i++) {
+        let key = array[i]
+        let j = i - 1
+        while (j >= 0 && key < array[j]) {
+            array[j + 1] = array[j]
+            j -= 1
+            array[j + 1] = key
         }
-    })
-    console.log(array)
+    }
+    return array
 }
 
-insertSort(unsortedArray)
+
+console.log(insertSort(unsortedArray1))
+console.log(insertSort(unsortedArray2))
+console.log(insertSort(unsortedArray3))
